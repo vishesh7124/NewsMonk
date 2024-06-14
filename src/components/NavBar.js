dbd201cbedb5a3bcd9c9e02f7563f0e1ad0139ef
Vishesh Gupta <visheshgupta065@gmail.com>
2024-06-14 19:54:43 +0530

    test

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -11,7 +11,7 @@ export class NavBar extends Component {
       <div>
         <nav className="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
             <div className="container-fluid">
-                <Link className="navbar-brand" to="/">NewsMonk</Link>
+                <Link className="navbar-brand" to="/">News Monk</Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
                 </button>
